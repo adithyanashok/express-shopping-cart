9e9466336c7da9d34692f07cfe64961a84f60942
Adithyan Ashok <adithyanashok406@gmail.com>
2021-06-29 10:39:48 +0530

    Session and Cookie

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var adminRouter = require('./routes/admin');
 var fileUpload = require('express-fileupload')
 var db = require('./config/connection')
 var app = express();
-
+var session = require('express-session')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -28,6 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+app.use(session({secret: "Key", cookie: {maxAge: 300000000000}}))
 db.connect((err) => {
   if(err) {
     console.log("Error "+err);
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,70 @@
 var express = require('express');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
+const userHelpers = require('../helpers/user-helpers')
+
+const verifyLogin = (req, res, next) => {
+  if(req.session.loggedIn) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const user = req.session.user
   productHelpers.getAllProducts().then((products) => {
-    res.render('index', { products, admin: false });
+    res.render('user/view-products', { products, admin: false, user });
   })
-
-  
 });
 
+// Login Get Method //
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/')
+  } else {
+    res.render('user/login', {"loginErr": req.session.loginErr})
+    req.session.loginErr = false
+  }
+  
+})
+
+// Login Post Method //
+router.post('/login', (req, res) => {
+  userHelpers.doLogin(req.body).then((response) => {
+    if(response.status) {
+      req.session.loggedIn = true
+      req.session.user = response.user
+      res.redirect('/')
+    } else {
+      req.session.loginErr = true
+      res.redirect('/login')
+      
+    }
+  })
+})
+
+// Signup Get Method //
+
+router.get('/signup', (req, res) => {
+  res.render('user/signup')
+})
+
+// Signup Post Method //
+router.post('/signup', (req, res) => {
+  userHelpers.doSignup(req.body).then((response) => {
+    console.log(response);
+  })
+})
+
+// Logout Method //
+router.get('/logout', (req, res) => {
+  req.session.destroy()
+  res.redirect('/')
+})
+
+// Get cart method //
+router.get('/cart', verifyLogin, (req, res) => {
+  res.render('user/cart')
+})
 module.exports = router;
